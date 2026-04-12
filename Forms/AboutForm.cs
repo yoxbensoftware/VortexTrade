daf9e75b3f5da4c8f8cd05573194ec1512b55995
Yağız Özgenç Özben <yagiz.ozben@rdc.com.tr>
2026-04-12 22:08:02 +0300

    V.0.0.9 - Hakkinda penceresi yerlesimi sadeleştirildi

--- Forms/AboutForm.cs
+++ Forms/AboutForm.cs
@@ -18,7 +18,7 @@ namespace VortexTrade
             var lblTitle = new Label
             {
                 Text = AppConstants.AppName,
-                Font = new Font("Consolas", 26f, FontStyle.Bold),
+                Font = new Font("Consolas", 24f, FontStyle.Bold),
                 ForeColor = accent,
                 AutoSize = true,
                 Location = new Point(24, 24)
@@ -28,9 +28,9 @@ namespace VortexTrade
             {
                 Text = $"Versiyon: {AppConstants.AppFullVersion}",
                 Font = new Font("Consolas", 10f),
-                ForeColor = fg,
+                ForeColor = Color.FromArgb(210, fg.R, fg.G, fg.B),
                 AutoSize = true,
-                Location = new Point(26, 68)
+                Location = new Point(24, lblTitle.Bottom + 10)
             };
 
             var lblDesc = new Label
@@ -41,7 +41,8 @@ namespace VortexTrade
                 Font = new Font("Consolas", 9f),
                 ForeColor = Color.FromArgb(160, fg.R, fg.G, fg.B),
                 AutoSize = true,
-                Location = new Point(26, 100)
+                MaximumSize = new Size(ClientSize.Width - 48, 0),
+                Location = new Point(24, lblVersion.Bottom + 18)
             };
 
             var btnOk = new Button
@@ -53,7 +54,7 @@ namespace VortexTrade
                 ForeColor = accent,
                 Font = new Font("Consolas", 9f),
                 Size = new Size(100, 32),
-                Location = new Point(300, 252)
+                Location = new Point(ClientSize.Width - 124, ClientSize.Height - 48)
             };
             btnOk.FlatAppearance.BorderColor = accent;
 
